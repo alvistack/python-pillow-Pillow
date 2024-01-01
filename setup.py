612fa92c20af230449289e826489800fe7ad0f48
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2024-01-01 20:05:44 +0800

    alvistack/10.2.0
        git clean -xdf
        tar zcvf ../python-pillow_10.2.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-pillow.spec ../python-pillow_10.2.0-1.spec
        cp ../python*-pil*10.2.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/python-pillow-Pillow-10.2.0/
        rm -rf ../python*-pil*10.2.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def get_version():
     return locals()["__version__"]
 
 
-PILLOW_VERSION = get_version()
+PILLOW_VERSION = "10.2.0"
 FREETYPE_ROOT = None
 HARFBUZZ_ROOT = None
 FRIBIDI_ROOT = None
